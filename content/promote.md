4c92e2d8c39e8093fd46fa1ddf79535625e2ecdc
azarai <azarai@codeboje.de>
2020-11-03 10:24:56 +0100

    added promotion page

---
+++ content/promote.md
@@ -0,0 +1,10 @@
+---
+slug: promote
+---
+
+# Advertise 📢
+
+Markdown powered page to write about your advertising opportunities.
+
+
+If you're interested in advertising with us, please email at SOCIAL_EMAIL for an enquiry.
---
+++ src/templates/StaticPage.vue
@@ -0,0 +1,46 @@
+<template>
+  <Layout>
+    <navBar />
+
+    <section class="text-black">
+      <div
+        class="container mx-auto flex px-5 py-4 items-center justify-center flex-col"
+      >
+
+        <div class="prose" v-html="$page.post.content.replace('SOCIAL_EMAIL', $static.metadata.socialEmail)"></div>
+        
+      </div>
+    </section>
+  </Layout>
+</template>
+
+<page-query>
+query ($id: ID!) {
+  post: staticPage(id: $id) {
+        id
+        title
+        content
+  }
+}
+</page-query>
+<static-query>
+query {
+  metadata {
+    socialEmail
+  }
+}
+</static-query>
+<script>
+import NavBar from "~/components/Navbar.vue";
+export default {
+  components: {
+    NavBar
+  },
+  metaInfo() {
+    return {
+      title: this.$page.post.title,
+    };
+  },
+
+};
+</script>
